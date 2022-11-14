7a83b27cce5dd3c1b7d6a595d4a223267f63249c
joshl26 <joshl26@hotmail.com>
2022-11-14 15:37:29 -0500

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import classes from "./Header.module.css";
 import logoImage from "../assets/tomato.svg";
 
+const logoLink = "/";
+
 const Header = () => {
   return (
     <header className={classes.container}>
       <div className={classes.content}>
-        <h1 className={classes.logoText}>
-          <a>
-            <img className={classes.logoImage} src={logoImage} />
-            Pomofocus Clone
-          </a>
-        </h1>
-        <p>Header</p>
+        <a href={logoLink} className={classes.logo_link}>
+          <img className={classes.logo_image} src={logoImage} />
+          <h1 className={classes.logo_text}>Pomofocus Clone</h1>
+        </a>
+        <ul className={classes.nav}></ul>
       </div>
     </header>
   );
