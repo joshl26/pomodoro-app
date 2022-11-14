19f83cacb9fa4e1d1c977731dce5a4dfb9f94310
joshl26 <joshl26@hotmail.com>
2022-11-14 12:16:20 -0500

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.container}>
+      <p>Header</p>
+    </div>
+  );
+};
+
+export default Header;
