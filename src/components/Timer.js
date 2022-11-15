88d2e6e8ea0e00e3630475b94b8a4b0bbd89490e
joshl26 <joshl26@hotmail.com>
2022-11-14 23:07:31 -0500

    commit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -9,8 +9,18 @@ const Timer = () => {
       <Progress percent={progress} />
       <div className={classes.container}>
         <div className={classes.content}>
-          <ul>{/* TODO */}</ul>
+          <ul>
+            <button className={classes.btn_secondary_active}>Pomodoro</button>
+            <button className={classes.btn_secondary}>Short Break</button>
+            <button className={classes.btn_secondary}>Long Break</button>
+          </ul>
+          <div className={classes.time}>25:00</div>
+          <div>
+            <button className={classes.action_btn}>START</button>
+          </div>
         </div>
+        <div className={classes.counter}>#1</div>
+        <footer>Time to Focus!</footer>
       </div>
     </div>
   );
