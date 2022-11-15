f6e7b284e4accca5988959eb8a5fd0d24fa3b58c
joshl26 <joshl26@hotmail.com>
2022-11-15 12:36:26 -0500

    commit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,37 @@
 import classes from "./Timer.module.css";
 import Progress from "./Progress";
 
+import { POMODORO, LONG_BREAK, SHORT_BREAK } from "../constants";
+
+const SecondaryButton = ({ label }) => {
+  return <button onClick={onClick}>{label}</button>;
+};
+
+const onClick = () => {
+  console.log("TEST");
+};
+
 const progress = "20%";
+
+const initialState = {
+  modes: {
+    [POMODORO]: {
+      id: POMODORO,
+      label: "Pomodoro",
+      time: 25,
+    },
+    [SHORT_BREAK]: {
+      id: SHORT_BREAK,
+      label: "Short Break",
+      time: 5,
+    },
+    [LONG_BREAK]: {
+      id: LONG_BREAK,
+      label: "Long Break",
+      time: 15,
+    },
+  },
+};
 
 const Timer = () => {
   return (
@@ -10,6 +40,7 @@ const Timer = () => {
       <div className={classes.container}>
         <div className={classes.content}>
           <ul>
+            <SecondaryButton label={"POMO"} />
             <button className={classes.btn_secondary_active}>Pomodoro</button>
             <button className={classes.btn_secondary}>Short Break</button>
             <button className={classes.btn_secondary}>Long Break</button>
