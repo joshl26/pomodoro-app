3f9418572bd148adfc082d13a9716b2406dabb75
joshl26 <joshl26@hotmail.com>
2022-11-21 21:20:14 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 
 // Importing the Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
